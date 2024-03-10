638e95585614b1eb9cf0742654d0c8cef4444e79
De-Lynn <k.riz98@mail.ru>
2024-03-10 21:30:40 +0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,11 @@ const nextConfig = {
     basePath: "/mir-vracha",
     output: "export",
     reactStrictMode: true,
-    images: {
-        path: "/mir-vracha"
-    },
+    
 };
 
 export default nextConfig
+
+// images: {
+//     path: "/mir-vracha"
+// },
